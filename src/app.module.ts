5b8a4166ad98c95d05fd75223280f4087196468f
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-06-21 19:36:41 -0300

    build: add shortUrlModule and DbModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { shortUrlModule } from './shortUrl/shortUrl.module';
+import { DbModule } from './database/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    shortUrlModule,
+    DbModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
