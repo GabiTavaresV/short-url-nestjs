cf8cd4b95242c964619ea133d65a60e74f785616
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-06-21 19:31:12 -0300

    feat: add DbModule

---
+++ src/database/db.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { databaseProviders } from "./providers";
+
+
+@Module({
+    providers: [...databaseProviders],
+    exports: [...databaseProviders]
+})
+
+export class DbModule {}
