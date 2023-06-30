423436c326a7b10015113dade00d6417cf408687
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-06-30 16:37:02 -0300

    build: update shortUrlModule

--- src/shortUrl/shortUrl.module.ts
+++ src/shortUrl/shortUrl.module.ts
@@ -1,16 +1,17 @@
 import { DbModule } from "src/database/db.module";
 import { ShortUrlController } from "./shortUrl.controller";
-import { shortUrlService } from "./shortUrl.service";
+import { ShortUrlService } from "./shortUrl.service";
 import { Module } from "@nestjs/common";
-import { shortUrlProviders } from "./shortUrl.providers";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ShortUrl, ShortUrlSchema } from "src/schema/url.shema";
 
 @Module({
-    imports: [DbModule],
+    imports: [MongooseModule.forFeature([
+      { name: ShortUrl.name, schema: ShortUrlSchema}
+    ])],
     controllers: [ShortUrlController],
-    providers: [
-      shortUrlService,
-      ...shortUrlProviders,
-    ],
+    providers: [ShortUrlService],
+    exports: [ShortUrlService]
   })
 
-  export class shortUrlModule{}
+  export class ShortUrlModule{}
