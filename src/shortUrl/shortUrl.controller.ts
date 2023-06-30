02e4c43cf4dba7918d6bf90b0d14277b4c4c21ad
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-06-30 16:38:11 -0300

    feat: update createShortUrl method, and add getShortUrl

--- src/shortUrl/shortUrl.controller.ts
+++ src/shortUrl/shortUrl.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { shortUrlService } from './shortUrl.service'
-import { CreateShortUrlDto } from "src/dto/create-shortUrl.dto";
+import { Body, Controller, Get, Param, Post, UsePipes } from "@nestjs/common";
+import { ShortUrlService } from './shortUrl.service'
+import { CreateShortUrlDto } from "./dto/create-shortUrl.dto";
+import { ValidationHttp } from "src/pipe/pipe.validation";
+import { ShortUrl } from "src/schema/url.shema";
 
 @Controller()
 export class ShortUrlController {
-    constructor(private readonly  shortUrlService: shortUrlService) {}
+    constructor(private readonly  shortUrlService: ShortUrlService) {}
 
     @Post()
-    async createShortUrl(@Body() input: CreateShortUrlDto) {
-        return this.shortUrlService.crateShortUrl(input);
+    async createShortUrl(@Body('url', new ValidationHttp()) url : CreateShortUrlDto ) {
+        return this.shortUrlService.crateShortUrl(url);
+    }
+
+    @Get(':code')
+    async getShortUrl(@Param('code') code : string ) {
+        return this.shortUrlService.getShortUrl(code)
     }
 }
